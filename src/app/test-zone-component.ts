1dc03b1199680fd03aa823f0ca5a89616c7fea6b
BARNOCH Anas <anasbarnoch@gmail.com>
2026-03-06 12:37:02 +0000

    example with signals

--- src/app/test-zone-component.ts
+++ src/app/test-zone-component.ts
@@ -1,17 +1,15 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, signal} from '@angular/core';
 
 @Component({
   selector: 'app-test-zone',
   standalone: true,
   template: `
-    <h1>Compteur : {{ compteur }}</h1>
+    <h1>Compteur : {{ compteur() }}</h1>
     <p>Statut : {{ statut }}</p>
   `
 })
 export class TestZoneComponent implements OnInit {
-  constructor(private cdr: ChangeDetectorRef) {
-  }
-  compteur = 0;
+  compteur = signal<number>(0);
   statut = 'Initialisé';
 
   ngOnInit() {
@@ -19,10 +17,9 @@ export class TestZoneComponent implements OnInit {
     // SANS Zone.js (Zoneless) : Le chiffre reste à 0 à l'écran,
     // même si la variable change en arrière-plan.
     setInterval(() => {
-      this.cdr.detectChanges(); // force detect changes
-      this.compteur++;
+      this.compteur.update(c => c + 1);
       this.statut = 'En cours...';
-      console.log('Valeur réelle du compteur :', this.compteur);
+      console.log('Valeur réelle du compteur :', this.compteur());
     }, 1000);
   }
 }
